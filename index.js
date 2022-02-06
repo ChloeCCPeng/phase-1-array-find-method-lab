14a8affd837ed6bf330fb811fcbd232555c40b1b
Chloe Peng <chloepeng331@icloud.com>
2022-02-06 13:01:54 -0500

    unfinished

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(record){
+    const win = record.find(result => result === 'W');
+        if (win === 'W'){
+            return record.year;
+        }
+}
